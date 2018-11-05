bebd78e60fc0daa6728be049f11f26b63949dbc6
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-04 19:16:06 -0900

    fixing cors for firefox

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', process.env.UI_URL);
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   next();
 });
 
