ee36978a82b5b797adac51b05d5d5e2ac28e1547
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-04 20:46:11 -0900

    send user bio with profile view

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -30,8 +30,10 @@ exports.explore = (req, res) => {
 
 // View Profile
 exports.profile = (req, res) => {
+  const bio = User.find({ username: req.params.username });
   Post.find({ username: req.params.username }).sort({ created: -1 })
-    .then(post => res.json(post.slice(parseInt(req.params.from, 10), parseInt(req.params.to, 10))))
+    .then(post => res.json(bio.bio, post.slice(parseInt(req.params.from, 10),
+      parseInt(req.params.to, 10))))
     .catch(() => res.status(500));
 };
 
